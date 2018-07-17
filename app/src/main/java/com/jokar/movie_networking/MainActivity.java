809735ed1b38453314dd2d5924c0427315abda8e
jokar13 <wivekks@gmail.com>
2018-07-18 04:03:04 +0530

    HttpURLConnection, openConnection(), Thread UI

--- app/src/main/java/com/jokar/movie_networking/MainActivity.java
+++ app/src/main/java/com/jokar/movie_networking/MainActivity.java
@@ -2,6 +2,12 @@ package com.jokar.movie_networking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                URL url = null;
+                try {
+                    url = new URL("http://google.com");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    Log.e("Response code", String.valueOf(urlConnection.getResponseCode()));
+                } catch (IOException e) {
+                    Log.d("error", e.getMessage(), e);
+                    e.printStackTrace();
+                }
+
+
+            }
+        }).start();
+
     }
 }
