70799c7afb9a1d86d9fa7747c699b98d78cedd9d
jokar13 <wivekks@gmail.com>
2018-07-18 05:34:54 +0530

    AsyncTask

--- app/src/main/java/com/jokar/movie_networking/MainActivity.java
+++ app/src/main/java/com/jokar/movie_networking/MainActivity.java
@@ -1,8 +1,10 @@
 package com.jokar.movie_networking;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -11,35 +13,52 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        new checkConnectionStatus().execute("http://google.com/");
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-
-                URL url = null;
-                try {
-                    url = new URL("http://google.com");
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                    Log.e("Response code", String.valueOf(urlConnection.getResponseCode()));
-                } catch (IOException e) {
-                    Log.d("error", e.getMessage(), e);
-                    e.printStackTrace();
-                }
-
-
-            }
-        }).start();
 
     }
+
+    class checkConnectionStatus extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            URL url = null;
+            try {
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+               return String.valueOf(urlConnection.getResponseCode());
+            } catch (IOException e) {
+                Log.d("error", e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mTextView = findViewById(R.id.Text);
+            mTextView.setText(s);
+        }
+
+
+
+    }
+
+
+
 }
