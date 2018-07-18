49dcda738dcaec855c977662f27f0a5241f2594c
jokar13 <wivekks@gmail.com>
2018-07-18 06:36:22 +0530

    POST Request.

--- app/src/main/java/com/jokar/movie_networking/MainActivity.java
+++ app/src/main/java/com/jokar/movie_networking/MainActivity.java
@@ -1,12 +1,20 @@
 package com.jokar.movie_networking;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -20,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new checkConnectionStatus().execute("http://google.com/");
+        new checkConnectionStatus().execute("http://www.androidinhindi.com/logindemo/login.php");
 
 
     }
@@ -30,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
+
+
             URL url = null;
             try {
                 url = new URL(params[0]);
@@ -39,10 +49,38 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-               return String.valueOf(urlConnection.getResponseCode());
+                urlConnection.setDoOutput(true);
+
+                // code starts here
+
+                //TODO : builder for getting the input
+                Uri.Builder mBuilder = new Uri.Builder()
+                        .appendQueryParameter("username", "admin")
+                        .appendQueryParameter("password", "admin");
+
+                //TODO : create OutputStream and BufferWriter
+                OutputStream outputStream = urlConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream),8);
+                bufferedWriter.write(mBuilder.build().getEncodedQuery());
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                urlConnection.connect();
+
+                //TODO : create InputStream and BufferReader
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String s = bufferedReader.readLine();
+                bufferedReader.close();
+
+                return s;
+
+
             } catch (IOException e) {
                 Log.d("error", e.getMessage(), e);
                 e.printStackTrace();
+
             }
 
             return null;
@@ -56,9 +94,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
     }
-
 
 
 }
